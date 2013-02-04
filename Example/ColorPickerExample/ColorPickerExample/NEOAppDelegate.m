e9e11a487193e2ea816b38aa973deec4b40d8f39
Karthik Abram <karthik.abram@neovera.com>
2013-02-04 10:16:09 -0500

    fixed issue #4

--- Example/ColorPickerExample/ColorPickerExample/NEOAppDelegate.m
+++ Example/ColorPickerExample/ColorPickerExample/NEOAppDelegate.m
@@ -16,7 +16,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[TestViewController alloc] initWithNibName:@"NEOViewController" bundle:nil];
+    self.viewController = [[TestViewController alloc] initWithNibName:@"TestViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
